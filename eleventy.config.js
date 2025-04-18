e3d58807ece0615d83366d9fa6002ce936af25a3
dreschers306 <dreschers306@gmail.com>
2025-04-18 14:46:09 +0800

    Configure passthrough copy for admin folder

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,18 @@
 module.exports = function(eleventyConfig) {
-    // Return your Object options:
-    return {
-      dir: {
-        input: "src", // Input directory
-        output: "_site", // Output directory
-        includes: "_includes", // Directory for reusable components/layouts
-        data: "_data" // Directory for global data files (like our videos.json later)
-      },
-      // Template formats to process (we'll start with Markdown and HTML)
-      templateFormats: ["md", "html", "njk"], // Added Nunjucks (.njk)
-      markdownTemplateEngine: "njk", // Use Nunjucks for Markdown files
-      htmlTemplateEngine: "njk" // Use Nunjucks for HTML files too
-    };
+
+  // Tell Eleventy to copy the 'admin' folder straight through
+  eleventyConfig.addPassthroughCopy("admin"); 
+
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src", 
+      output: "_site", 
+      includes: "_includes", 
+      data: "_data" 
+    },
+    templateFormats: ["md", "html", "njk"], 
+    markdownTemplateEngine: "njk", 
+    htmlTemplateEngine: "njk" 
   };
+};
