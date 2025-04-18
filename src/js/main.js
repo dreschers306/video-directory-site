b68a253a9abe32af48b1da12caac40660e521a78
dreschers306 <dreschers306@gmail.com>
2025-04-18 19:32:32 +0800

    Layout: Default mobile to 1 column, Debug: Disable randomization

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 // Function to shuffle an array in place (Fisher-Yates algorithm)
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-    }
-  }
+// function shuffleArray(array) {
+//    for (let i = array.length - 1; i > 0; i--) {
+//      const j = Math.floor(Math.random() * (i + 1));
+//      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+//    }
+//  }
   
   // Wait for the HTML document to be fully loaded
   document.addEventListener('DOMContentLoaded', () => {
